4f002fb4ea59da8457bd8f7fd98990609c931229
Bill Taggart <taggarw@gmail.com>
2018-03-03 23:42:37 -0700

    add CSS & Javascript - Thumbnails Only

---
+++ index.js
@@ -0,0 +1,35 @@
+const YOUTUBE_SEARCH_URL ='https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromYouTube(criteria, callback) {
+	const searchQuery = {
+		part: 'snippet',
+		maxResults: 25,
+		q: criteria,
+		key: 'AIzaSyCSWmkSbWWBVMOke76KasHlgxY4CMmmjUQ'
+	}
+	$.getJSON(YOUTUBE_SEARCH_URL,searchQuery,callback);
+}
+
+function renderResults(result) {
+	return `
+	<div class="result">
+		<img src="${result.snippet.thumbnails.medium.url}">
+	</div>`
+}
+
+function youTubeSearchData(data) {
+	const results = data.items.map((item,index) => renderResults(item));
+	$(".search-results").html(results);
+}
+
+function searchSubmit() {
+	$(".search-form").submit(event => {
+		event.preventDefault();
+		const searchQuery = $(event.currentTarget).find(".search-query");
+		const query = searchQuery.val();
+		searchQuery.val("");
+		getDataFromYouTube(query,youTubeSearchData);
+	});
+}
+
+$(searchSubmit);
