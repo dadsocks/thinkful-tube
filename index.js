6234c5dba2b8a01158117bda97afd356f0269a52
Bill Taggart <taggarw@gmail.com>
2018-03-04 17:44:59 -0700

    Fixed File Links & Added Lightbox Functionality

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function getDataFromYouTube(criteria, callback) {
 function renderResults(result) {
 	return `
 	<div class="result">
-		<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank">
+		<a href="https://www.youtube.com/embed/${result.id.videoId}" class="open-lightbox" target="_blank">
 			<img src="${result.snippet.thumbnails.medium.url}">
 		</a>
 	</div>`
@@ -34,4 +34,40 @@ function searchSubmit() {
 	});
 }
 
+function renderLightBox(vidID) {
+	return `
+	<div class="lightbox">
+		<p>CLick Anywhere to Close</p>
+    	<div class="content">
+        	<iframe src="${vidID}"  width="641" height="360" frameborder="0" allow="autoplay; encrypted-media"></iframe>
+    	</div>
+	</div>`
+}
+
+function closeLightBox() {
+	$("html").on("click",".lightbox", function(event) {
+		event.preventDefault();
+		$(".lightbox").hide();
+	});
+}
+
+function lightBoxDisplay() {
+	$("main").on("click", ".open-lightbox", function(event) {
+		event.preventDefault();
+		const videoHref = $(event.currentTarget).attr("href");
+		const lightbox = renderLightBox(videoHref);
+		$('body').append(lightbox);
+	});
+	closeLightBox();
+}
+
+
+
+$(lightBoxDisplay);
 $(searchSubmit);
+
+
+
+
+
+
